c100c68e236db328cc8096a4663cf9eca49eeef7
AmazingBandit <11012938+sagun-r@users.noreply.github.com>
2024-02-20 21:42:39 -0800

    Added formatting for cards when added. Added basic format for book detail section

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const myLibrary = [];
-const p = document.querySelector("p");
+const myLibrary = [["Harry Potter and the Sorcerer's Stone"], ["J.K. Rowling"], ["309"], ["238"]];
+[["Life of Pi"], ["Yann Martel"], ["352"], ["352"]];
+[["Daughter of Fortune"], ["Isabel Allende"], ["496"], ["72"]];
+
+const p = document.querySelector(".empty-msg");
 const addBtn = document.querySelector(".add-btn");
 const mainContainer = document.querySelector(".main-container");
 
@@ -7,18 +10,27 @@ addBtn.addEventListener("click", () => {
   p.remove();
   const cardContainer = document.createElement("div");
   cardContainer.classList.add("card-container");
+  const bookTitle = document.createElement("p");
+  bookTitle.classList.add("book-title");
+  const bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("book-author");
+  const totalPage = document.createElement("p");
+  totalPage.classList.add("total-page");
+  const pageRead = document.createElement("p");
+  pageRead.classList.add("page-read");
 
   mainContainer.append(cardContainer);
+  cardContainer.append(bookTitle, bookAuthor, totalPage, pageRead);
 });
 
-const Book = () => {
-  for (book of myLibrary) {
-    p.textContent += book + " ";
-  }
-};
+// const Book = () => {
+//   for (book of myLibrary) {
+//     p.textContent += book + " ";
+//   }
+// };
 
-const addBookToLibrary = () => {};
-Book();
+// const addBookToLibrary = () => {};
+// Book();
 
 const login = document.querySelector(".login");
 // login.addEventListener("click", () => {});
